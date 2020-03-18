50eb1fa5c01c311705dcbdc14839f29fd5a08f69
yuzhanglong <yuzl1123@163.com>
2020-03-18 16:47:11 +0800

    处理个人中心

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ App({
     const token = wx.getStorageSync(TOKEN);
     if (token) {
       this.checkToken(token);
+      this.getUserInfo();
     } else {
       this.weChatLogin();
     }
@@ -50,5 +51,33 @@ App({
         });
       }
     });
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          this.getUserInfo();
+        } else {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success() {
+              // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+              this.getUserInfo();
+            }
+          })
+        }
+      }
+    })
+  },
+
+  getUserInfo() {
+    wx.getUserInfo({
+      success: res => {
+        // 可以将 res 发送给后台解码出 unionId
+        console.log(res);
+        this.globalData.userInfo = res.userInfo
+      }
+    })
   }
 });
--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,11 +1,22 @@
 // pages/profile/profile.js
+
+const app = getApp();
+
+
 Page({
-  data: {},
+  data: {
+    userInfo: {}
+  },
 
   onLoad: function () {
     //TODO: 个人中心页面
     // 隐藏小房子
     wx.hideHomeButton();
+
+    // 拉取个人信息
+    this.setData({
+      userInfo: app.globalData.userInfo
+    });
   },
 
 });
